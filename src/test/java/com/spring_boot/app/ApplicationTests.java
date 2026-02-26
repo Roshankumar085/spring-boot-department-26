f69099d14c1c5f0267514a18368d7499dbe32688
ROSHAN KUMAR <rkbcet1997@gmail.com>
2026-02-27 02:55:26 +0530

    Add Prometheus and Grafana monitoring setup

--- src/test/java/com/spring_boot/app/ApplicationTests.java
+++ src/test/java/com/spring_boot/app/ApplicationTests.java
@@ -1,13 +1,13 @@
-//package com.spring_boot.app;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class ApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package com.spring_boot.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
